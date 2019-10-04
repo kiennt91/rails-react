0df47e6ab32b316a8b80d4e53d485967e65d5c6e
kien4c <kien4c@gmail.com>
2019-10-04 16:31:07 +0900

    exampleの追加

---
+++ app/javascript/packs/App.js
@@ -0,0 +1,60 @@
+import React, { Component, Suspense } from 'react';
+import { useTranslation, withTranslation, Trans } from 'react-i18next';
+import './i18n';
+
+// use hoc for class based components
+class LegacyWelcomeClass extends Component {
+  render() {
+    const { t, i18n } = this.props;
+    return <h2>{t('title')}</h2>;
+  }
+}
+const Welcome = withTranslation()(LegacyWelcomeClass);
+
+// Component using the Trans component
+function MyComponent() {
+  return (
+    <Trans i18nKey="description.part1">
+      To get started, edit <code>src/Home.js</code> and save to reload.
+    </Trans>
+  );
+}
+
+// page uses the hook
+function Page() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <div className="Home">
+      <div className="Home-header">
+        <Welcome />
+        <button onClick={() => changeLanguage('de')}>de</button>
+        <button onClick={() => changeLanguage('en')}>en</button>
+      </div>
+      <div className="Home-intro">
+        <MyComponent />
+      </div>
+      <div>{t('description.part2')}</div>
+    </div>
+  );
+}
+
+// loading component for suspense fallback
+const Loader = () => (
+  <div className="Home">
+    <div>loading...</div>
+  </div>
+);
+
+// here app catches the suspense from page in case translations are not yet loaded
+export default function Home() {
+  return (
+    <Suspense fallback={<Loader />}>
+      <Page />
+    </Suspense>
+  );
+}
